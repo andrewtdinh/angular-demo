0b24edb432d6d527c48345c9096ab4fcf1ee9f45
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-20 12:58:21 -0700

    regular commit

--- client/index.js
+++ client/index.js
@@ -2,8 +2,45 @@
 
 'use strict';
 
-$(document).ready(init);
+angular.module('alpha', [])
+.controller('gamma', ['$scope', function($scope){
+  console.log('beta controller initialized');
+  $scope.x = 5;
+  $scope.y = 7;
+}])
+.controller('beta', ['$scope', function($scope){
+  console.log('beta controller initialized');
+  $scope.x = 3;
+  $scope.y = 2;
+}])
+.run(['$rootScope', function($rootScope){
+  console.info('runnning the gun function');
+  $rootScope.z = 12;
+}])
+.controller('calculator', ['$scope', function($scope){
+  console.info('calculator controller initialized');
+  $scope.compute = function(){
+    console.warn('we just clicked the green compute button');
 
-function init(){
+    var result;
+    var x = $scope.x;
+    var y = $scope.y;
 
-}
+    switch($scope.op){
+      case '+':
+        result = x + y;
+        break;
+      case '-':
+        result = x - y;
+        break;
+      case '*':
+        result = x * y;
+        break;
+      case '/':
+        result = x / y;
+        break;
+    }
+    $scope.result = result;
+  };
+
+}]);
